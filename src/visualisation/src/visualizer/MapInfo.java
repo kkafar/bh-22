af1226ccd5e79db377ee3d70acc68807b078ae3d
Marcin Hawryluk <70582973+mhawryluk@users.noreply.github.com>
2022-01-15 23:45:02 +0100

    Integracja wizualizacji z bazą danych (#7)
    * Add visualization basic template
    
    * Add heatMap drawing
    
    * Rysowanie traktora + poprawienie wizualizacji mapy
    
    * Mapowanie wartości na saturację koloru
    
    * Dodanie translacji punktów podanych we współrzędnych geograficznych
    
    * Dodanie JetPanel
    
    * Wysyłanie zapytania do serwera o dane
    
    * Odczytywanie JSONa
    
    * Integracja rysowania mapy z bazą danych

--- src/visualisation/src/visualizer/MapInfo.java
+++ src/visualisation/src/visualizer/MapInfo.java
@@ -1,29 +1,79 @@
 package visualizer;
 
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
-import java.util.List;
+
 
 public class MapInfo {
 
-    public Point upperLeft = new Point(0, 0);
-    public Point lowerRight = new Point(1000, 600);
+    public Point upperRight = new Point(0, 0);
+    public Point lowerLeft = new Point(1000, 600);
 
-    private int [][] map;
+    private URL url;
+    private double [][] map;
+    private ArrayList<Point> sensors = new ArrayList<>();
 
-    public MapInfo(){
-        map = new int[][]{
-                {-1, -1, 0, 0, 2, 0, 0, 0, 0, 0},
-                {-1, -1, 0, 2, 3, 2, 0, 0, 0, 0},
-                {0, 0, 0, 2, 3, 4, 3, 2, 0, 0},
-                {0, 0, 1, 0, 2, 3, 2, 0, 0, 0},
-                {0, 0, 0, 0, 0, 2, 0, 0, -1, -1},
-                {0, 0, 1, 0, 0, 0, 0, 0, -1, -1}
-        };
+    private final int clientID = 1;
+    private final int mapID = 1;
+
+    private final Gson gson = new Gson();
+
+    public MapInfo() {
+
+        try {
+            connect();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void connect() throws IOException{
+        url = new URL("http://10.230.128.212:5000");
+
+        URLConnection con = url.openConnection();
+        HttpURLConnection http = (HttpURLConnection)con;
+        http.setRequestMethod("POST");
+        http.setDoOutput(true);
+
+        byte[] out = ("{\"clientId\":\"" + clientID + "\",\"mapId\":\"" + mapID +"\"}").getBytes(StandardCharsets.UTF_8);
+        int length = out.length;
+
+        http.setFixedLengthStreamingMode(length);
+        http.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+        http.connect();
+        try(OutputStream os = http.getOutputStream()) {
+            os.write(out);
+        }
+
+        byte[] bytes = http.getInputStream().readAllBytes();
+        String response = new String(bytes, StandardCharsets.UTF_8);
+
+        ResponseJson responseJson = gson.fromJson(response, ResponseJson.class);
+
+        System.out.println(responseJson);
+        map = responseJson.heatMap;
+        lowerLeft = new Point(responseJson.lowerLeft[0], responseJson.lowerLeft[1]);
+        upperRight = new Point(responseJson.upperRight[0], responseJson.upperRight[1]);
+
+        System.out.println(lowerLeft);
+        System.out.println(upperRight);
+
+        double[][] sensorArray = responseJson.sensors;
+        for (int i = 0; i < sensorArray.length; i++){
+            sensors.add(new Point(sensorArray[i][0], sensorArray[i][1]));
+        }
     }
 
     public Point translate(Point point, int mapWidth, int mapHeight){
-        int translatedX = (int)(point.x/(lowerRight.x - upperLeft.x)*mapWidth);
-        int translatedY = (int)(point.y/(lowerRight.y - upperLeft.y)*mapHeight);
+        int translatedX = (int)((point.x - lowerLeft.x)/(upperRight.x - lowerLeft.x)*mapWidth);
+        int translatedY = (int)((point.y - lowerLeft.y)/(upperRight.y - lowerLeft.y)*mapHeight);
 
         return new Point(translatedX, translatedY);
     }
@@ -37,20 +87,14 @@ public class MapInfo {
     }
 
     public ArrayList<Point> getSensors(){
-        ArrayList<Point> points =  new ArrayList<Point>(List.of(new Point[]{
-                new Point(100, 200),
-                new Point(400, 500),
-                new Point(300, 200)
-        }));
-
-        return points;
+        return sensors;
     }
 
-    public int [][] getHeatMap(){
+    public double [][] getHeatMap(){
         return map;
     }
 
     public Point getTractorPosition(){
-        return new Point(500, 300);
+        return new Point(63, 45);
     }
 }
