bc9eeec5578e644c50894a05c7aa69371a937723
Marcin Hawryluk <70582973+mhawryluk@users.noreply.github.com>
2022-01-15 19:43:35 +0100

    visualization part 1! (#4)
    * Add visualization basic template
    
    * Add heatMap drawing
    
    * Rysowanie traktora + poprawienie wizualizacji mapy
    
    * Mapowanie wartości na saturację koloru
    
    * Dodanie translacji punktów podanych we współrzędnych geograficznych
    
    * Dodanie JetPanel

---
+++ src/visualisation/src/visualizer/MapPanel.java
@@ -0,0 +1,124 @@
+package visualizer;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+
+public class MapPanel extends JPanel implements ActionListener {
+
+    public final int width;
+    public final int height;
+
+    private final int squareWidth;
+    private final int squareHeight;
+
+    private final int tractorSize = 50;
+    private final Image tractorImage = new ImageIcon("pics/tractor.png").getImage().getScaledInstance(tractorSize, tractorSize, Image.SCALE_DEFAULT);
+
+    private final Timer timer;
+    private boolean heatMapShowing = false;
+
+    private final MapInfo mapInfo = new MapInfo();
+    private final int[][] heatMap = mapInfo.getHeatMap();
+    private int maxValue;
+    private final ArrayList<Point> sensors;
+
+    public MapPanel(int width, int height){
+        this.width = width;
+        this.height = height;
+
+        squareHeight = height/heatMap.length;
+        squareWidth = width/heatMap[0].length;
+
+        maxValue = -1;
+
+        for (int i = 0; i < heatMap.length; i++){
+            for (int j = 0; j < heatMap[0].length; j++){
+                maxValue = Math.max(maxValue, heatMap[i][j]);
+            }
+        }
+
+        sensors = mapInfo.translate(mapInfo.getSensors(), width, height);
+
+        setBounds(0, 0, width, height);
+        setPreferredSize(new Dimension(width, height));
+        setLayout(null);
+
+        timer = new Timer(50, this);
+        timer.start();
+
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                heatMapShowing = !heatMapShowing;
+            }
+        });
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        Graphics2D g2D = (Graphics2D) g;
+        super.repaint();
+
+        // background
+        g2D.setPaint(new Color(88, 92, 107));
+        g2D.fillRect(0, 0, this.width, this.height);
+
+        // map
+        for (int i = 0; i < heatMap.length; i++){
+            for (int j = 0; j < heatMap[0].length; j++){
+                if (heatMap[i][j] < 0) g2D.setPaint(new Color(88, 92, 107));
+                else g2D.setPaint(new Color(229, 193, 165));
+                g2D.fillRect(j*squareWidth, i*squareHeight, squareWidth, squareHeight);
+            }
+        }
+
+        // heatmap
+        if (heatMapShowing) {
+            for (int i = 0; i < heatMap.length; i++) {
+                for (int j = 0; j < heatMap[0].length; j++) {
+                    if (heatMap[i][j] < 0) continue;
+                    g2D.setPaint(getHeatHue(heatMap[i][j]));
+                    g2D.fillRect(j * squareWidth, i * squareHeight, squareWidth, squareHeight);
+                }
+            }
+        }
+
+        // grid
+        g2D.setPaint(new Color(225, 217, 217));
+        for (int i = 0; i < heatMap.length; i++){
+            for (int j = 0; j < heatMap[0].length; j++){
+                g2D.drawRect(j*squareWidth, i*squareHeight, squareWidth, squareHeight);
+            }
+        }
+
+        // sensors
+        g2D.setPaint(new Color(21, 36, 143));
+        for (Point sensor: sensors){
+            g2D.fillOval((int)sensor.x, (int)sensor.y, 12, 12);
+        }
+
+        //tractor
+        Point tractorPos = mapInfo.translate(mapInfo.getTractorPosition(), width, height);
+        if (tractorPos != null){
+            g2D.drawImage(tractorImage, (int)(tractorPos.x - tractorSize/2), (int)(tractorPos.y - tractorSize/2), tractorSize, tractorSize, null,this);
+        }
+    }
+
+    private Color getHeatHue(int value){
+        Color color = Color.getHSBColor(225, (float)value/(float)maxValue*100, 100);
+        System.out.println(color);
+        return Color.getHSBColor((float)(225./360.), (float)value/(float)maxValue, 1);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == timer) {
+
+        }
+    }
+}
---
+++ src/visualisation/src/visualizer/Window.java
@@ -0,0 +1,36 @@
+package visualizer;
+import javax.swing.*;
+import java.awt.*;
+
+public class Window extends JFrame {
+
+    public final int width = 1000;
+    public final int height = 800;
+
+    public static void main(String[]  args){
+        new Window();
+    }
+
+    public Window() {
+        setTitle("Fertisave");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setLayout(new FlowLayout());
+
+        setBounds(0, 0, width, height+50);
+        setPreferredSize(new Dimension(width, height+50));
+
+        MapPanel mapPanel = new MapPanel(width, 3*height/4);
+        add(mapPanel);
+
+        JetPanel jetPanel = new JetPanel(width, height/4);
+        add(jetPanel);
+
+        pack();
+
+        Dimension screenDim = Toolkit.getDefaultToolkit().getScreenSize();
+        setLocation(screenDim.width/2 - getSize().width/2, screenDim.height/2 - getSize().height/2);
+
+        setResizable(false);
+        setVisible(true);
+    }
+}
