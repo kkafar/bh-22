f0b721f0a0824434f99d5d9bfbdf03d1e0201d49
mhawryluk <marcinhawrylukxx@gmail.com>
2022-01-15 17:10:02 +0100

    Add heatMap drawing

--- src/visualisation/src/visualizer/MapInfo.java
+++ src/visualisation/src/visualizer/MapInfo.java
@@ -7,30 +7,29 @@ public class MapInfo {
 
     public ArrayList<Point> getSensors(){
         return new ArrayList<Point>(List.of(new Point[]{
-                new Point(1, 2),
-                new Point(4, 5),
-                new Point(3, 2)
+                new Point(100, 200),
+                new Point(400, 500),
+                new Point(300, 200)
         }));
     }
 
-    public ArrayList<Point> getMapVertices(){
-        return null;
-    }
-
     public int [][] getHeatMap(){
-        return null;
-    }
+        int[][] map = {
+                {-1, -1, 0, 0, 0, 0, 0, 0, 0, 0},
+                {-1, -1, 0, 0, 0, 0, 0, 0, 0, 0},
+                {-1, -1, 0, 0, 0, 0, 1, 0, 0, 0},
+                {-1, -1, 0, 0, 3, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 3, 4, 3, 0, 0, 0},
+                {0, 0, 1, 0, 0, 3, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 4, 0, 0, -1, -1},
+                {0, 0, 1, 0, 0, 0, 0, 0, -1, -1}
+        };
 
-    public int getHeatMapWidth(){
-        return 20;
-    }
-
-    public int getHeatMapHeight(){
-        return 10;
+        return map;
     }
 
     public Point getTractorPosition(){
         return new Point(0, 0);
     }
-
 }
--- src/visualisation/src/visualizer/MapPanel.java
+++ src/visualisation/src/visualizer/MapPanel.java
@@ -4,18 +4,31 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
 
 public class MapPanel extends JPanel implements ActionListener {
 
     private final int width;
     private final int height;
+
+    private int squareWidth;
+    private int squareHeight;
+
     private final Timer timer;
+    private boolean heatMapShowing = false;
 
     private final MapInfo mapInfo = new MapInfo();
+    private final int[][] heatMap = mapInfo.getHeatMap();
 
     public MapPanel(int width, int height){
+
         this.width = width;
         this.height = height;
+
+        squareHeight = height/heatMap.length;
+        squareWidth = width/heatMap[0].length;
 
         setBounds(0, 0, width, height);
         setPreferredSize(new Dimension(width, height));
@@ -23,6 +36,13 @@ public class MapPanel extends JPanel implements ActionListener {
 
         timer = new Timer(50, this);
         timer.start();
+
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                heatMapShowing = !heatMapShowing;
+            }
+        });
     }
 
     @Override
@@ -33,6 +53,35 @@ public class MapPanel extends JPanel implements ActionListener {
         // background
         g2D.setPaint(new Color(236, 195, 161));
         g2D.fillRect(0, 0, this.width, this.height);
+
+
+        // sensors
+        g2D.setPaint(new Color(0, 220, 206));
+        ArrayList<Point> sensors = mapInfo.getSensors();
+        for (Point sensor: sensors){
+            g2D.fillOval((int)sensor.x, (int)sensor.y, 10, 10);
+        }
+
+        // map
+        for (int i = 0; i < heatMap.length; i++){
+            for (int j = 0; j < heatMap[0].length; j++){
+                if (heatMap[i][j] < 0) g2D.setPaint(new Color(88, 92, 107));
+                else g2D.setPaint(new Color(229, 193, 165));
+                g2D.fillRect(j*squareWidth, i*squareHeight, squareWidth, squareHeight);
+            }
+        }
+
+        // heatmap
+        if (heatMapShowing) {
+            for (int i = 0; i < heatMap.length; i++) {
+                for (int j = 0; j < heatMap[0].length; j++) {
+                    if (heatMap[i][j] < 0) continue;
+
+                    g2D.setPaint(new Color(0, heatMap[i][j] * 30, 0, 100));
+                    g2D.fillRect(j * squareWidth, i * squareHeight, squareWidth, squareHeight);
+                }
+            }
+        }
     }
 
     @Override
