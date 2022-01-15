5a865da5dab32e44bad8762fd025ae3a67b5fdb2
mhawryluk <marcinhawrylukxx@gmail.com>
2022-01-15 21:07:46 +0100

    Wysyłanie zapytania do serwera o dane

--- src/visualisation/src/visualizer/MapInfo.java
+++ src/visualisation/src/visualizer/MapInfo.java
@@ -1,5 +1,12 @@
 package visualizer;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,9 +15,20 @@ public class MapInfo {
     public Point upperLeft = new Point(0, 0);
     public Point lowerRight = new Point(1000, 600);
 
+    private URL url;
     private int [][] map;
 
-    public MapInfo(){
+    private final int clientID = 1;
+    private final int mapID = 1;
+
+    public MapInfo() {
+
+        try {
+            connect();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         map = new int[][]{
                 {-1, -1, 0, 0, 2, 0, 0, 0, 0, 0},
                 {-1, -1, 0, 2, 3, 2, 0, 0, 0, 0},
@@ -19,6 +37,28 @@ public class MapInfo {
                 {0, 0, 0, 0, 0, 2, 0, 0, -1, -1},
                 {0, 0, 1, 0, 0, 0, 0, 0, -1, -1}
         };
+    }
+
+    private void connect() throws IOException{
+        url = new URL("http://10.230.128.212:5000");
+
+        URLConnection con = url.openConnection();
+        HttpURLConnection http = (HttpURLConnection)con;
+        http.setRequestMethod("POST");
+        http.setDoOutput(true);
+
+        byte[] out = ("{\"clientId\":\"" + clientID + "\",\"mapId\":\"" + mapID +"\"}").getBytes(StandardCharsets.UTF_8);
+        int length = out.length;
+
+        http.setFixedLengthStreamingMode(length);
+        http.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+        http.connect();
+        try(OutputStream os = http.getOutputStream()) {
+            os.write(out);
+        }
+
+        byte[] bytes = http.getInputStream().readAllBytes();
+        System.out.println(new String(bytes, StandardCharsets.UTF_8));
     }
 
     public Point translate(Point point, int mapWidth, int mapHeight){
